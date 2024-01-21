dae4ea0efa243d98251506d95b96750ffb283823
Adamo  kehinde hassan <102253825+lambojac@users.noreply.github.com>
2024-01-21 23:36:28 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
 import React from "react"
  export default function App(){
-  return <div>hello how aree you doing it been a while</div>
+  return <div>hello how aree you doing it been a while am keny a software developer</div>
  }
